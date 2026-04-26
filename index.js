ba2c46697e8eb9d8aab4b41a95d5d2bca091cc8d
sukazxcmeow <sukazxcmeow@gmail.com>
2026-04-26 21:21:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const server = net.createServer((socket) => {
   socket.once('data', (data) => {
